22997a1846ea33dce2cc92ae752e9a780f41c68b
Theodore Alwin Wason <theowason2k21@gmail.com>
2025-02-17 08:35:05 +0530

    Update ReadFileByLine.java

--- w3_IOStreams/ReadFileByLine.java
+++ w3_IOStreams/ReadFileByLine.java
@@ -6,7 +6,7 @@ import java.io.IOException;
 
 public class ReadFileByLine {
     public static void main(String[] args) {
-        String filePath =  "C:\\Users\\STENI DEV S A\\Downloads\\UNIT 3 WNBA ANS[1].pdf";
+        String filePath =  "C:\\Users\\THEODORE ALWIN WASON\\Downloads\\UNIT 3 WNBA ANS[1].pdf";
 
         try (BufferedReader reader = new BufferedReader(new FileReader(filePath))) {
             String line;
